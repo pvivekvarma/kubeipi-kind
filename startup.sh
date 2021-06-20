9480750d05a5b4fae8ac9427e5b46da78ab530e2
Vivek Varma <vvarma@mypassglobal.com>
2021-06-20 07:54:16 +0000

    Added installation and setup of kubernetes dashboard

--- startup.sh
+++ startup.sh
@@ -5,3 +5,8 @@ bash -c '/usr/local/bin/kind create cluster --image kindest/node:v1.17.0 --name 
 echo Modifying Kubernetes config to point to Kind master node
 MASTER_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' pi-kube-control-plane)
 sed -i "s/^    server:.*/    server: https:\/\/$MASTER_IP:6443/" $HOME/.kube/config
+
+echo Deploying Kubernetes dashboard and create a dashboard service account
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.2.0/aio/deploy/recommended.yaml
+kubectl create serviceaccount dashboard-admin-sa
+kubectl create clusterrolebinding dashboard-admin-sa --clusterrole=cluster-admin --serviceaccount=default:dashboard-admin-sa
